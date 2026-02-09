b33ea13b225f53d016fee689f9b10decb9d8ea07
FSWLJY <fanshiwei0709@163.com>
2026-02-09 11:39:09 +0800

    Add hello.cpp test

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cout << "Hello, Algorithm!" << endl;
+    int a = 1, b = 2;
+    cout << "1 + 2 = " << a + b << endl;
+    return 0;
+}
